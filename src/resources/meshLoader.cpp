cab346f3683e59cd5b54cc7e011a8fad6c0e2bf8
x1nixmzeng <x1nixmzeng@live.co.uk>
2024-02-17 22:00:55 +0000

    Add assert when LoadObj fails

--- src/resources/meshLoader.cpp
+++ src/resources/meshLoader.cpp
@@ -2,11 +2,16 @@
 
 #include "resources/rlobj.h"
 
+#include <cassert>
+
 MeshResPtr MeshLoader::loadMesh(const std::string& filename) {
     MeshRes* mesh = new MeshRes();
 
     // instead use https://github.com/Not-Nik/rlobj
     mesh->model = LoadObj(filename.c_str());
+
+    // models will silently fail to load if files cannot be found
+    assert(mesh->model.meshCount > 0);
 
     return MeshResPtr(mesh);
 }
